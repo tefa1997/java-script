7dd7f262672ebbc74480eba81b278d3af6cf18fd
tefa1997 <pao_sanchezs@hotmail.com>
2026-04-29 00:17:00 -0500

    feat: combiné funciones para mostrar en consola y pantalla

--- script.js/script.js
+++ script.js/script.js
@@ -1,44 +1,31 @@
 function ejercicio1() {
     alert("Hello World");
+    console.log("Hello World");
 }
 
 function ejercicio2() {
     document.getElementById("resultado").innerHTML = "Hello World";
-}
-
-function ejercicio3() {
-    document.getElementById("resultado").innerHTML = 3 + 5;
-}
-
-function ejercicio4() {
-    let nombreUsuario = prompt("¿Cuál es tu nombre?");
-    document.getElementById("resultado").innerHTML = "Hola " + nombreUsuario;
-}
-
-function ejercicio5() {
-    let numero6 = prompt("Ingresa el primer número:");
-    let numero9 = prompt("Ingresa el segundo número:");
-    document.getElementById("resultado").innerHTML = "La suma es: " + (Number(numero6) + Number(numero9));
-}
-function ejercicio1() {
-    alert("Hello World");
-}
-
-function ejercicio2() {
     console.log("Hello World");
 }
 
 function ejercicio3() {
+    document.getElementById("resultado").innerHTML = 3 + 5;
     console.log(3 + 5);
 }
 
 function ejercicio4() {
     let nombreUsuario = prompt("¿Cuál es tu nombre?");
+    document.getElementById("resultado").innerHTML = "Hola " + nombreUsuario;
     console.log("Hola " + nombreUsuario);
 }
 
 function ejercicio5() {
     let numero1 = prompt("Ingresa el primer número:");
     let numero2 = prompt("Ingresa el segundo número:");
-    console.log("La suma es: " + (Number(numero1) + Number(numero2)));
+    let suma = Number(numero1) + Number(numero2);
+    document.getElementById("resultado").innerHTML = "La suma es: " + suma;
+    console.log("La suma es: " + suma);
 }
+
+
+
