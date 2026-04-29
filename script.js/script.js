d416984d896b3c0c1f2ba7aa7975ec921ae4eec2
tefa1997 <pao_sanchezs@hotmail.com>
2026-04-29 00:09:41 -0500

    fix: corregí variables en suma

--- script.js/script.js
+++ script.js/script.js
@@ -20,3 +20,25 @@ function ejercicio5() {
     let numero9 = prompt("Ingresa el segundo número:");
     document.getElementById("resultado").innerHTML = "La suma es: " + (Number(numero6) + Number(numero9));
 }
+function ejercicio1() {
+    alert("Hello World");
+}
+
+function ejercicio2() {
+    console.log("Hello World");
+}
+
+function ejercicio3() {
+    console.log(3 + 5);
+}
+
+function ejercicio4() {
+    let nombreUsuario = prompt("¿Cuál es tu nombre?");
+    console.log("Hola " + nombreUsuario);
+}
+
+function ejercicio5() {
+    let numero1 = prompt("Ingresa el primer número:");
+    let numero2 = prompt("Ingresa el segundo número:");
+    console.log("La suma es: " + (Number(numero1) + Number(numero2)));
+}
